ab6afc03099e52ce8fffa6b868feebb1be89580b
Gopal Ojha <gopalonevirus@gmail.com>
2021-01-20 15:36:16 +0545

    adding submit transaction and workload
    Complete Initial setup for caliper with fabric 2.2 and caliper 0.4.2

--- caliper-workspace/workload/readAsset.js
+++ caliper-workspace/workload/readAsset.js
@@ -9,14 +9,49 @@ class MyWorkload extends WorkloadModuleBase {
     
     async initializeWorkloadModule(workerIndex, totalWorkers, roundIndex, roundArguments, sutAdapter, sutContext) {
         await super.initializeWorkloadModule(workerIndex, totalWorkers, roundIndex, roundArguments, sutAdapter, sutContext);
+
+        for (let i=0; i<this.roundArguments.assets; i++) {
+            const assetID = `${this.workerIndex}_${i}`;
+            console.log(`Worker ${this.workerIndex}: Creating asset ${assetID}`);
+            const request = {
+                contractId: this.roundArguments.contractId,
+                contractFunction: 'CreateAsset',
+                invokerIdentity: 'User1',
+                contractArguments: [assetID,'blue','20','penguin','500'],
+                readOnly: false
+            };
+            await this.sutAdapter.sendRequests(request);
+        }
     }
     
     async submitTransaction() {
-        // NOOP
+        const randomId = Math.floor(Math.random()*this.roundArguments.assets);
+        const myArgs = {
+            contractId: this.roundArguments.contractId,
+            contractFunction: 'ReadAsset',
+            invokerIdentity: 'User1',
+            contractArguments: [`${this.workerIndex}_${randomId}`],
+            readOnly: true
+        };
+        
+        console.log("\n submitTransaction() User1 ReadAsset \t ") 
+        await this.sutAdapter.sendRequests(myArgs);
     }
     
     async cleanupWorkloadModule() {
-        // NOOP
+        for (let i=0; i<this.roundArguments.assets; i++) {
+            const assetID = `${this.workerIndex}_${i}`;
+            console.log(`Worker ${this.workerIndex}: Deleting asset ${assetID}`);
+            const request = {
+                contractId: this.roundArguments.contractId,
+                contractFunction: 'DeleteAsset',
+                invokerIdentity: 'User1',
+                contractArguments: [assetID],
+                readOnly: false
+            };
+
+            await this.sutAdapter.sendRequests(request);
+        }
     }
 }
 
@@ -25,3 +60,32 @@ function createWorkloadModule() {
 }
 
 module.exports.createWorkloadModule = createWorkloadModule;
+
+// 'use strict';
+
+// const { WorkloadModuleBase } = require('@hyperledger/caliper-core');
+
+// class MyWorkload extends WorkloadModuleBase {
+//     constructor() {
+//         super();
+//     }
+    
+//     async initializeWorkloadModule(workerIndex, totalWorkers, roundIndex, roundArguments, sutAdapter, sutContext) {
+//         await super.initializeWorkloadModule(workerIndex, totalWorkers, roundIndex, roundArguments, sutAdapter, sutContext);
+//     }
+    
+//     async submitTransaction() {
+//         // NOOP
+//     }
+    
+//     async cleanupWorkloadModule() {
+//         // NOOP
+//     }
+// }
+
+// function createWorkloadModule() {
+//     console.log("\n we are in createWorkLoadModule \n ...............\n ................\n ")
+//     return new MyWorkload();
+// }
+
+// module.exports.createWorkloadModule = createWorkloadModule;
